4ec1af64342d9d72925323f6d30ca332092d24b4
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2024-08-11 16:00:18 -0400

    Delete MetricValues.java

--- src/main/java/edu/s3/qmood4j/metrics/MetricValues.java
+++
@@ -1,9 +0,0 @@
-package edu.s3.qmood4j.metrics;
-
-import java.util.HashMap;
-
-public class MetricValues extends HashMap<MetricProperty, Double> {
-
-    private static final long serialVersionUID = -4123494242735124258L;
-
-}
