40c890cb61c21982d5cc3197bd5ea194cf633db5
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2024-08-18 20:04:29 -0400

    removed unused code

--- src/main/java/edu/s3researchlab/qmood4j/metrics/Metric.java
+++ src/main/java/edu/s3researchlab/qmood4j/metrics/Metric.java
@@ -1,7 +1,5 @@
 package edu.s3researchlab.qmood4j.metrics;
 
-import java.util.Map;
-
 import edu.s3researchlab.qmood4j.model.ClassModel;
 import edu.s3researchlab.qmood4j.model.ProjectModel;
 
@@ -27,9 +25,5 @@ public abstract class Metric {
         }
 
         return total / pm.getNumberOfClasses();
-    }
-
-    public double calculate(Map<MetricName, Double> metricValues) {
-        return 0.0;
     }
 }
